661d199a6e9d94316022ba5bcd55a7753187cc9c
Anupaud <115567419+Anupaud@users.noreply.github.com>
2022-10-14 22:53:15 +0530

    Assignment05
    Assignment05

---
+++ Csharp/Assignment05/Assignment05/IStudent.cs
@@ -0,0 +1,93 @@
+﻿//4.Create an Interface IStudent with int StudentId and string Name as Properties, void ShowDetails() as its method.
+//   Create 2 classes Dayscholar and Resident that implements the interface Properties and Methods.ShowDetails() should show
+//    DaysScholar details and Resident Details accordingly. 
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment05
+{
+    interface IStudent
+    {
+        int StudentId { get; set; }
+        string Name { get; set; }
+        void ShowDetatils();
+    }
+
+    public class DayScholar : IStudent
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+
+        public int _StudentId
+        {
+            set { StudentId = value; }
+            get { return StudentId; }
+        }
+
+        public string _Name
+        {
+            set { Name = value; }
+            get { return Name; }
+        }
+
+        public void GetDayScholar()
+        {
+            Console.Write("Enter DayScholar Student Id :");
+            StudentId = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter DayScholar Student Name : ");
+            Name = Console.ReadLine();
+        }
+        public void ShowDetatils()
+        {
+            Console.WriteLine("  Day Scholar Student Details are: ");
+            Console.WriteLine("StudentId : {0}\nName : {1}", StudentId, Name);
+        }
+    }
+
+    public class Resident : IStudent
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+
+        public int _StudentId
+        {
+            set { StudentId = value; }
+            get { return StudentId; }
+        }
+
+        public string _Name
+        {
+            set { Name = value; }
+            get { return Name; }
+        }
+
+        public void GetResident()
+        {
+            Console.Write("Enter Resident Student Id :");
+            StudentId = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Resident Student Name : ");
+            Name = Console.ReadLine();
+        }
+
+        public void ShowDetatils()
+        {
+            Console.WriteLine("  Resident Student Details are: ");
+            Console.WriteLine("StudentId : {0}\nName : {1}", StudentId, Name);
+        }
+    }
+    class Student_InterfaceEg
+    {
+        static void Main(string[] args)
+        {
+            Resident obj1 = new Resident();
+            obj1.GetResident();
+            obj1.ShowDetatils();
+            DayScholar obj2 = new DayScholar();
+            obj2.GetDayScholar();
+            obj2.ShowDetatils();
+            Console.Read();
+        }
+    }
+}
---
+++ Csharp/Assignment05/Assignment05/Passanger.cs
@@ -0,0 +1,36 @@
+﻿//2.Create an console application to book train tickets. Create a Passanger class with (Name, Age) and 
+//    write a function called TicketBooking(int no_of_tickets) that takes no.of tickets to be booked. 
+//    If the no of tickets is > 2 per booking, raise an user defined exception, and print "cannot book more than 2 tickets".
+//Else Print  "Ticket Booked Successfully". Add a Test class to call TicketBooking method by accepting all required details.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment05
+{
+    class Passanger
+    {
+        public int Age;
+        public string Name;
+        private int no_of_tickets;
+
+        public void TicketBooking(int no_of_tickets)
+        {
+            if (no_of_tickets > 2)
+            {
+                Console.WriteLine("cannot book more than 2 tickets");
+            }
+            else
+            {
+                Console.WriteLine("Ticket Booked Successfully");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Passanger obj = new Passanger();
+            
+        }
+    }
+   
+}
