af7a016de3f2c4dd7890d842f69dc2f6c87f5234
Anupaud <115567419+Anupaud@users.noreply.github.com>
2022-10-14 22:48:23 +0530

    Assignment03
    Assignment03

---
+++ Csharp/Assignment03/Assignment03/Accounts.cs
@@ -0,0 +1,89 @@
+﻿//1.Create a class called Accounts which has data members(fields) like Account no, Customer name, Account type, Transaction type (d/w), amount, balance
+//D->Deposit
+//W->Withdrawal
+
+//-write a function that updates the balance depending upon the transaction type
+
+//	-If transaction type is deposit call the credit(int amount) function and update balance
+//	-If transaction type is withdraw call debit(int amt)function and update balance
+//-Pass the other information like Acount no, name, acc type through constructor
+//-call the show data method to display the values.
+
+using System;
+
+namespace Assignment03
+{
+    class Accounts
+    {
+        
+        public int AccountNo;
+        public string Customername;
+        public string AccountType;
+        public char TransactionType;
+        public int Amount;
+        public int Balance ;
+        public char d = 'd';
+        public char w = 'w';
+
+
+        static void Main(string[] args)
+        {
+            Accounts bank = new Accounts(668877, "sheshadri", "savingsAcc");
+            Console.WriteLine("Please enter the transaction Type (d for deposit / w for withdrawal) :");
+            bank.TransactionType = Convert.ToChar(Console.ReadLine()); 
+            Console.WriteLine("Enter the amount : ");
+            bank.Amount = Convert.ToInt32(Console.ReadLine());
+            bank.Balance = bank.TransType(bank.Amount, bank.TransactionType);
+            Console.WriteLine("After the Transaction the Account Details are : ");
+            bank.ShowData();
+           Console.ReadKey();
+        }
+
+        public Accounts(int accNo, string name, string accType)
+        {
+            AccountNo = accNo;
+            Customername = name;
+            AccountType = accType;
+        }
+
+        public int TransType(int amt, char transactionType)
+        {
+            if (transactionType == d)
+            {
+                Console.WriteLine("Amount Credited into your account : " + amt);
+                return AmountCredit(amt);
+            }
+
+            else if (transactionType == w)
+            {
+                Console.WriteLine("Amount Debited from your account: " + amt);
+                return AmountDebit(amt);
+            }
+
+            else
+            {
+                Console.WriteLine("Enter valid Transaction Type : ");
+                return 0;
+            }
+
+        }
+
+        public int AmountCredit(int amountValue)
+        {
+            Balance = Balance + amountValue;
+            return Balance;
+        }
+
+        public int AmountDebit(int amountValue)
+        {
+            Balance = Balance - amountValue;
+            return Balance;
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("Account No :{0}\nCustomer Name :{1}\nAccount type : {2}\nBalance : {3}", AccountNo, Customername, AccountType, Balance);
+        }
+    }
+}
+
---
+++ Csharp/Assignment03/Assignment03/Student.cs
@@ -0,0 +1,98 @@
+﻿//2.Create a class called student which has data members like rollno, name, class, Semester, branch, int[] marks = new int marks [5](marks of 5 subjects )
+
+//-Pass the details of student like rollno, name, class, SEM, branch in constructor
+//- For marks write a method called GetMarks() and give marks for all 5 subjects
+//-Write a method called displayresult, which should calculate the average marks
+//-If marks of any one subject is less than 35 print result as failed
+//-If marks of all subject is >35 but average is < 50 then also print result as failed
+//-If avg > 50 then print result as passed.
+//-Write a DisplayData() method to display all values.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment03
+{
+    class Student
+    {
+          
+			public int RollNo;
+			public string Name;
+			public int stuClass;
+			public int Semester;
+			public string branch;
+			public int sum;
+			int[] marks = new int[5];
+			int avgMarks;
+			int count = 0;
+		
+
+		public Student(int rolNo, string stName, int sClass, int sem, string branchName)
+			{
+				RollNo = rolNo;
+				Name = stName;
+				stuClass = sClass;
+				Semester = sem;
+				branch = branchName;
+
+			}
+
+			public void GetMarks()        
+			{
+
+				for (int i = 0; i < marks.Length; i++)
+				{
+					marks[i] = Convert.ToInt32(Console.ReadLine());
+					if (marks[i] < 35)
+					{
+						count++;
+					}
+					sum += marks[i];
+
+				}
+
+			}
+
+			public void DisplayResult()     
+			{
+				int len = marks.Length;
+				int sum = 0;
+				for (int i = 0; i < marks.Length; i++)
+				{
+					sum += marks[i];
+				}
+
+				avgMarks = sum / len;
+			}
+
+			public void DisplayData()
+			{
+				Console.WriteLine("Student Details And Results are:");
+				Console.WriteLine("Student Name :{0}\nStudent Roll No :{1}\nStudent class :{2}\nsemester :{3}\nbranch is :{4}", Name, RollNo, stuClass, Semester, branch);
+				Console.WriteLine("total marks :{0}\nAvg Marks : {1}:", sum, avgMarks);
+
+				if (count > 0 && avgMarks < 50)
+				{
+					Console.WriteLine("Student {0} got failed ", Name);
+				}
+				else
+				{
+					Console.WriteLine("Student {0} got Passed ", Name);
+				}
+			}
+		static void Main(string[] args)
+		{
+			Student obj = new Student(101, "sheshadri", 10, 6, "science");
+			Console.WriteLine("Enter the marks of all five subjects of the student  :");
+			obj.GetMarks();
+			obj.DisplayResult();
+			obj.DisplayData();
+
+			Console.ReadKey();
+		}
+
+
+	}
+}
+
